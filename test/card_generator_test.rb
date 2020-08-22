b9694ea9f085fa286affa962d5bfa34d7b0d9b86
Kevin Cuadros <kev040798@gmail.com>
2020-08-22 15:36:53 -0400

    Card generator done

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -4,16 +4,24 @@ require '../lib/card_generator'
 
 class CardGeneratorTest < Minitest::Test
   def test_it_exists
-    filename = "cards.txt"
+    filename = "../data/cards.txt"
     cards = CardGenerator.new(filename)
 
     assert_instance_of CardGenerator, cards
   end
 
   def test_it_returns_filename
-    filename = "cards.txt"
+    filename = "../data/cards.txt"
     cards = CardGenerator.new(filename)
 
-    assert_equal "cards.txt", cards.filename
+    assert_equal "../data/cards.txt", cards.filename
+  end
+
+  def test_it_returns_array_of_cards
+
+    filename = "../data/cards.txt"
+    cards = CardGenerator.new(filename)
+
+    assert_equal 52, cards.cards.length
   end
 end
